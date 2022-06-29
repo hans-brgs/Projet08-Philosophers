db260d6efe9e36502d677becd9bae5237153cf2b
hans <hans.bourgeois@ulb.be>
2022-06-29 09:23:05 +0200

    06-29-2022 09:23:05 | 1st Push

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -14,30 +14,41 @@
 
 int parse_arg(int argc, char *argv[], t_philo *philo)
 {
-	philo->num_of_philo = ft_atoi(argv[1]);
-	philo->time_to_die = ft_atoi(argv[2]);
-	philo->time_to_eat = ft_atoi(argv[3]);
-	philo->time_to_sleep = ft_atoi(argv[4]);
+	if (philo->num_of_philo = ft_atoi(argv[1]) < 1);
+		return (-1);
+	if (philo->time_to_die = ft_atoi(argv[2]) < 1);
+		return (-1);
+	if (philo->time_to_eat = ft_atoi(argv[3]) < 1);
+		return (-1);
+	if (philo->time_to_sleep = ft_atoi(argv[4]) < 1);
+	 	return (-1);
 
 	if (argc == 6)
-		philo->num_dishes_eaten = ft_atoi(argv[5]);
-
+		if (philo->num_dishes_eaten = ft_atoi(argv[5]) < 1);
+			return (-1);
 	return (1);
 }
 
 int main(int argc, char *argv[])
 {
 	t_philo *philo;
+	int check;
 
 	philo = malloc(sizeof(t_philo));
 	if (!philo)
 		return (-1);
 	if (argc == 5 || argc == 6)
-		 parse_arg(argc, argv, philo);
+		 check = parse_arg(argc, argv, philo);
 
-	printf("num of philo = %ld\n", philo->num_of_philo);
-	printf("time to die = %ld\n", philo->time_to_die);
-	printf("time to eat = %ld\n", philo->time_to_eat);
-	printf("time to sleep = %ld\n", philo->time_to_sleep);
-	printf("num dishes eaten = %ld\n", philo->num_dishes_eaten);
+	if (check > 0) 
+	{
+		printf("num of philo = %ld\n", philo->num_of_philo);
+		printf("time to die = %ld\n", philo->time_to_die);
+		printf("time to eat = %ld\n", philo->time_to_eat);
+		printf("time to sleep = %ld\n", philo->time_to_sleep);
+		printf("num dishes eaten = %ld\n", philo->num_dishes_eaten);
+	}
+	else 
+		printf("wrong argument\n", philo->num_of_philo);
+	
 }
